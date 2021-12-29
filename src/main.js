34be1258fb7a62486e17dff0e3626df685eaab24
antoinefa <fabien.antoine@m4x.org>
2021-12-29 20:45:18 +0100

    migrate vue-shortkey to vue3-shortkey

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueShortkey from 'vue-shortkey'
+import VueShortkey from 'vue3-shortkey'
 import VueClipboard from 'vue-clipboard2'
 import Codemirror from 'codemirror-editor-vue3'
 
